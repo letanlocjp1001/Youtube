79fc32d84756faad6926b0acfca0be51934bf776
letanloc <letanlocjp1001@gmail.com>
2023-10-10 21:33:46 +0900

    project

---
+++ Accordion/js/app.js
@@ -0,0 +1,21 @@
+const questions = document.querySelectorAll('.question')
+
+questions.forEach((question) => {
+  question.addEventListener('click', () => {
+    const questionActive = document.querySelector('.question.active')
+
+    if (questionActive && questionActive !== question) {
+      questionActive.classList.toggle('active')
+      questionActive.nextElementSibling.style.maxHeight = 0
+    }
+
+    question.classList.toggle('active')
+
+    const textContent = question.nextElementSibling
+    if (question.classList.contains('active')) {
+      textContent.style.maxHeight = textContent.scrollHeight + 'px'
+    } else {
+      textContent.style.maxHeight = 0
+    }
+  })
+})
